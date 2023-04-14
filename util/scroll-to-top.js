aa4ec084584fa81d0a7d48219b7a3c38e77cb129
tbenson89 <typrobenson@gmail.com>
2023-04-14 16:10:29 -0600

    video section updated - notes added NEXT steps

---
+++ util/scroll-to-top.js
@@ -0,0 +1,19 @@
+// Get the button
+var myButton = document.getElementById("scrollBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        myButton.style.display = "block";
+    } else {
+        myButton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    window.scrollTo({ top: 0, behavior: 'smooth' }); // For Chrome, Firefox, IE and Opera
+}
